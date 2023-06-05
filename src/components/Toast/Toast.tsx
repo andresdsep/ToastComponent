927e4ddad45f6a4e1fac7d35c6be02ebfaebd083
Andres Sepulveda <a.sepulveda@valdperformance.com>
2023-06-05 13:58:53 +1000

    feat: Upgrade Toast to .ts

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import {
-  AlertOctagon,
-  AlertTriangle,
-  CheckCircle,
+  // AlertOctagon,
+  // AlertTriangle,
+  // CheckCircle,
   Info,
   X,
 } from 'react-feather';
-
 import VisuallyHidden from '../VisuallyHidden';
-
 import styles from './Toast.module.css';
 
-const ICONS_BY_VARIANT = {
-  notice: Info,
-  warning: AlertTriangle,
-  success: CheckCircle,
-  error: AlertOctagon,
-};
+// const ICONS_BY_VARIANT = {
+//   notice: Info,
+//   warning: AlertTriangle,
+//   success: CheckCircle,
+//   error: AlertOctagon,
+// };
 
 function Toast() {
   return (
